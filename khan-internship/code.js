a5c825267cbf9ce592a87c0eb5e9523649360002
Kasra Kyanzadeh <me@kasrak.com>
2014-12-16 23:32:42 -0500

    autoplay videos

--- khan-internship/code.js
+++ khan-internship/code.js
@@ -1,60 +1,71 @@
+(function() {
 "use strict";
+/*jslint browser: true */
 
-function shuffleArray(array) {
-    // In-place Fisher-Yates
-    var i;
-    for (i = array.length - 1; i > 0; i--) {
-        var j = Math.round(Math.random() * i);
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
-
-function randomizeList(listElement) {
-    var listItems = Array.prototype.slice.call(
-        listElement.getElementsByTagName("li"));
-
-    shuffleArray(listItems);
-
-    listItems.forEach(function(item) {
-        listElement.removeChild(item);
-        listElement.appendChild(item);
-    });
-}
-
-function horizontallyCenterOnPage(element) {
+function forEach(seq, fn) {
+    Array.prototype.forEach.call(seq, fn);
 }
 
 function applyToElementsWithClassName(className, fn) {
-    Array.prototype.forEach.call(
-        document.getElementsByClassName(className),
-        fn);
+    forEach(document.getElementsByClassName(className), fn);
 }
 
-function setUpVideoPlayer(videoPlayer) {
-    var videoElements = videoPlayer.getElementsByTagName("video");
-    var playButton = videoPlayer.getElementsByClassName("play-button")[0];
-    var pauseButton = videoPlayer.getElementsByClassName("pause-button")[0];
-    var playingClass = "playing";
+function VideoPlayer(container) {
+    var self = this;
+    this.userPaused = false;
+    this.isPlaying = false;
+    this.container = container;
+    this.videoElements = container.getElementsByTagName("video");
+    this.playButton = container.getElementsByClassName("play-button")[0];
+    this.pauseButton = container.getElementsByClassName("pause-button")[0];
 
-    playButton.onclick = function() {
-        videoPlayer.classList.add(playingClass);
-        Array.prototype.forEach.call(videoElements, function(video) {
-            video.play();
-        });
+    this.playButton.onclick = function() {
+        self.play();
     };
 
-    pauseButton.onclick = function() {
-        videoPlayer.classList.remove(playingClass);
-        Array.prototype.forEach.call(videoElements, function(video) {
-            video.pause();
-        });
+    this.pauseButton.onclick = function() {
+        self.userPaused = true;
+        self.pause();
     };
+}
+
+VideoPlayer.PlayingClass = "playing";
+
+VideoPlayer.prototype.play = function() {
+    if (this.isPlaying) {
+        return;
+    }
+    this.isPlaying = true;
+    this.container.classList.add(VideoPlayer.PlayingClass);
+    forEach(this.videoElements, function(video) {
+        video.play();
+    });
+};
+
+VideoPlayer.prototype.pause = function() {
+    if (!this.isPlaying) {
+        return;
+    }
+    this.isPlaying = false;
+    this.container.classList.remove(VideoPlayer.PlayingClass);
+    forEach(this.videoElements, function(video) {
+        video.pause();
+    });
+};
+
+VideoPlayer.prototype.autoPlay = function() {
+    if (!this.userPaused) {
+        this.play();
+    }
+};
+
+var videoPlayers = [];
+function setUpVideoPlayer(container) {
+    videoPlayers.push(new VideoPlayer(container));
 }
 
 var overlayBackground = document.createElement("div");
-function toggleOverlayWithId(id) {
+window.toggleOverlayWithId = function(id) {
     var overlay = document.getElementById(id);
     var classList = overlay.classList;
     var visibleClass = "visible";
@@ -83,32 +94,75 @@ function toggleOverlayWithId(id) {
             document.body.removeChild(overlayBackground);
         }
     }
-}
+};
 
-function debounce(fn, delay) {
-    var timer;
+function throttle(fn, delay) {
+    var timer = null;
     var debounced = function() {
+        if (timer) {
+            return;
+        }
         var args = arguments;
-        window.clearTimeout(timer);
         timer = window.setTimeout(function() {
             fn.apply(null, args);
+            timer = null;
         }, delay);
-    }
+    };
     return debounced;
 }
 
-window.addEventListener("load", function() {
-    applyToElementsWithClassName("randomize-list", randomizeList);
-    applyToElementsWithClassName("horizontally-center-on-page", horizontallyCenterOnPage);
-    applyToElementsWithClassName("video-player", setUpVideoPlayer);
-});
+function first(array) {
+    return array[0];
+}
 
-var scrollChanged = debounce(function() {
-    var windowTop = window.scrollY;
-    var windowBottom = windowTop + window.innerHeight;
-    // Pause all videos that aren't in the viewport
-    // Play the videos that are in the viewport?
-}, 50);
+function last(array) {
+    return array[array.length - 1];
+}
+
+
+var lastScrollY = 0;
+var scrollingDown = 1, scrollingUp = -1;
+var scrollDirection = scrollingDown;
+var scrollChanged = throttle(function() {
+    if (Math.abs(lastScrollY - window.scrollY) < 20) {
+        // Ignore small changes.
+        return;
+    }
+
+    scrollDirection = (lastScrollY <= window.scrollY) ? scrollingDown : scrollingUp;
+    lastScrollY = window.scrollY;
+
+    var videoPlayersInViewport = [];
+    videoPlayers.forEach(function(videoPlayer) {
+        var rect = videoPlayer.container.getBoundingClientRect();
+        var inViewport = (rect.bottom > 0) &&
+            (rect.top < window.innerHeight) &&
+            (rect.top > 0) &&
+            (rect.bottom < window.innerHeight);
+
+        if (inViewport) {
+            videoPlayersInViewport.push(videoPlayer);
+        } else {
+            videoPlayer.pause();
+        }
+    });
+
+    var videoToPlay = ((scrollDirection == scrollingDown) ? last : first)(videoPlayersInViewport);
+    videoPlayersInViewport.forEach(function(videoPlayer) {
+        if (videoPlayer == videoToPlay) {
+            videoPlayer.autoPlay();
+        } else {
+            videoPlayer.pause();
+        }
+    });
+}, 100);
+
 window.addEventListener("scroll", function() {
     scrollChanged();
 });
+
+window.addEventListener("load", function() {
+    applyToElementsWithClassName("video-player", setUpVideoPlayer);
+});
+
+})();
